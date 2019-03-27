6b95817131ef0c0a80a3b6bfafbbdb1fc9915b94
Israel Merida Lopez <1717110625@utectulancingo.edu.mx>
2019-03-27 09:54:45 -0600

    base de batos remota

--- application/models/config.py
+++ application/models/config.py
@@ -3,10 +3,10 @@ import web
 print"conectando"
 db = web.database(
     dbn='mysql', # motor de base de datos
-    host='localhost', # ruta del servidor
-    db='ferreteria_iml', # nombre de la base de datos
-    user='root', # usuario dela base de datos
-    pw='1234', # password del usuario
+    host='cig4l2op6r0fxymw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', # ruta del servidor
+    db='x5mr0c1om4knotua', # nombre de la base de datos
+    user='zpj10ffxpxk38w9b', # usuario dela base de datos
+    pw='zm314m31xnd3pzua', # password del usuario
     port = 3306 # puerto de mariadb
     )
 print"conectado"
